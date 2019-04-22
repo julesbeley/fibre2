817bb2262008ca155e366633d2a16332310343cf
Jules Beley <jules.beley@gmail.com>
2019-04-22 12:08:09 +0200

    maps function finished

--- minicartes.R
+++ minicartes.R
@@ -11,26 +11,23 @@ place <- function(search) {
     url <- "https://api-adresse.data.gouv.fr/search" 
     query <- GET(url, query = list(q = search, limit = 1))
     geojson <- read_sf(content(query, as = "text"))
-    out <- cbind(geojson[ ,c(1:2)],
+    out <- cbind(geojson[, c(1:2)],
                  geojson$geometry[[1]][1],
                  geojson$geometry[[1]][2])
     colnames(out) <- c("label", "score", "long", "lat", "geometry")
     out$geometry <- NULL
-    return(out) 
+    France <- map_data("world", region = "France")
+    ggplot() +
+        geom_polygon(data = France, 
+                     aes(x = long, y = lat, group = group), 
+                     fill = NA, 
+                     color = "black") +
+        theme_void() +
+        coord_map(projection = "lambert", 
+                  parameters = c(10, 49)) +
+        geom_point(data = out,
+                   aes(x = long, y = lat)) -> plot
+    return(list(out, plot))
 }
 
-lyon <- place("Lyon")
-
-ggplot() +
-    geom_polygon(data = France, 
-                 aes(x = long, y = lat, group = group), 
-                 fill = NA, 
-                 color = "black") +
-    theme_void() +
-    coord_map(projection = "lambert", 
-              parameters = c(10, 49)) +
-    geom_point(data = lyon,
-               aes(x = long, y = lat))
-
-
-
+place("dasle")
